787cd5f80f0ce88eff043819f7b110bcf9fe6151
MaixBat <mixa2001ff@gmail.com>
2022-11-19 02:12:02 +0400

    Hands, Bug fixes
    Добавлены руки, больше не нужно подбирать лопату, теперь нельзя вскопать уже копанное место

--- Assets/Scripts/UseScript.cs
+++ Assets/Scripts/UseScript.cs
@@ -17,9 +17,8 @@ public class UseScript : MonoBehaviour
     [SerializeField] Terrain ter;
     [SerializeField] GameObject terrain;
 
-    [SerializeField] GameObject TextGive;
-    [SerializeField] GameObject Lopata;
     [SerializeField] GameObject LopataInHand;
+    [SerializeField] GameObject Hands;
     [SerializeField] GameObject Dirt;
 
 
@@ -66,46 +65,50 @@ public class UseScript : MonoBehaviour
         RaycastHit HitObject;
         Ray CenterScreen = MovePlayer.Player.Povorot.GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
-        TextGive.SetActive(false);
+        if (CheckMove == false)
+        {
+            Hands.SetActive(false);
+            LopataInHand.SetActive(true);
+        }
+        else
+        {
+            LopataInHand.SetActive(false);
+            Hands.SetActive(true);
+        }
 
         if (Physics.Raycast(CenterScreen, out HitObject, DistanceGive))
         {
-            if (HitObject.collider == Lopata.GetComponent<BoxCollider>())
-            {
-                TextGive.SetActive(true);
-                if (Input.GetKey(KeyCode.E))
-                {
-                    Lopata.SetActive(false);
-                    LopataInHand.SetActive(true);
-                }
-            }
             if (HitObject.collider == terrain.GetComponent<TerrainCollider>())
             {
-                if (LopataInHand.activeInHierarchy && Input.GetMouseButtonDown(0) && CheckMove)
+                PointX = Convert.ToInt32(HitObject.point.x * MapResol);
+                PointZ = Convert.ToInt32(HitObject.point.z * MapResol);
+                if (Heights[PointZ, PointX] >= HeightMapDefault)
                 {
-                    PointX = Convert.ToInt32(HitObject.point.x * MapResol);
-                    PointZ = Convert.ToInt32(HitObject.point.z * MapResol);
-
-                    DOTween.Sequence()
-                        .AppendCallback(CheckMoveVoidF)
-                        .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.3784409f, -0.05f, 0.15f), duration: 0.5f))
-                        .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(28.554f, 7.702f, 27.442f), duration: 0.3f))
-                        .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.2678188f, -0.3367314f, 0.830399f), duration: 0.1f))
-                        .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-1.71f, -2.496f, 22.479f), duration: 0.1f))
-                        .AppendCallback(DirtTrue)
-                        .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.295495f, -0.4635951f, 0.9054654f), duration: 0.3f))
-                        .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-33.098f, -11.056f, 27.706f), duration: 0.4f))
-                        .AppendCallback(SetHeights)
-                        .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-38.21f, -56.401f, 73.332f), duration: 0.2f))
-                        .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(-0.2987438f, -0.4296662f, 1.049282f), duration: 0.5f))
-                        .Join(Dirt.transform.DOLocalMove(endValue: new Vector3(-3.89f, 2.71f, 1.49f), duration: 0.7f))
-                        .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(14.877f, 6.996f, 25.548f), duration: 0.3f))
-                        .Join(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.37844f, -0.502f, 0.827f), duration: 1f))
-                        .AppendCallback(DirtFalse)
-                        .Append(Dirt.transform.DOLocalMove(endValue: new Vector3(0.004f, 0.068f, 1.333f), duration: 0.01f))
-                        .SetLoops(3)
-                        .AppendCallback(CheckMoveVoidT);
-                    DepthGround = StartDepthGround;
+                    Hands.SetActive(false);
+                    LopataInHand.SetActive(true);
+                    if (Input.GetMouseButtonDown(0) && CheckMove)
+                    {
+                        DOTween.Sequence()
+                            .AppendCallback(CheckMoveVoidF)
+                            .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.3784409f, -0.05f, 0.15f), duration: 0.5f))
+                            .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(28.554f, 7.702f, 27.442f), duration: 0.3f))
+                            .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.2678188f, -0.3367314f, 0.830399f), duration: 0.1f))
+                            .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-1.71f, -2.496f, 22.479f), duration: 0.1f))
+                            .AppendCallback(DirtTrue)
+                            .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.295495f, -0.4635951f, 0.9054654f), duration: 0.3f))
+                            .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-33.098f, -11.056f, 27.706f), duration: 0.4f))
+                            .AppendCallback(SetHeights)
+                            .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(-38.21f, -56.401f, 73.332f), duration: 0.2f))
+                            .Append(LopataInHand.transform.DOLocalMove(endValue: new Vector3(-0.2987438f, -0.4296662f, 1.049282f), duration: 0.5f))
+                            .Join(Dirt.transform.DOLocalMove(endValue: new Vector3(-3.89f, 2.71f, 1.49f), duration: 0.7f))
+                            .Append(LopataInHand.transform.DOLocalRotate(endValue: new Vector3(14.877f, 6.996f, 25.548f), duration: 0.3f))
+                            .Join(LopataInHand.transform.DOLocalMove(endValue: new Vector3(0.37844f, -0.502f, 0.827f), duration: 1f))
+                            .AppendCallback(DirtFalse)
+                            .Append(Dirt.transform.DOLocalMove(endValue: new Vector3(0.004f, 0.068f, 1.333f), duration: 0.01f))
+                            .SetLoops(3)
+                            .AppendCallback(CheckMoveVoidT);
+                        DepthGround = StartDepthGround;
+                    }
                 }
             }
         }
